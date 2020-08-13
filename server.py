9238dad15e8eaf7a0607d4200e2182e21eedba53
balaji38738 <bijjapwar123@gmail.com>
2020-08-13 12:53:58 +0530

    [Balaji] Add.Code for receiving data from client

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import socket
 server_socket = socket.socket()
 print('Socket Created')
 server_socket.bind(('localhost', 9999))
-server_socket.listen(3)
+server_socket.listen(10)
 print('Waiting for connections')
 
 while True:
     client_socket, address = server_socket.accept()
+    name = client_socket.recv(1024).decode()
     print('Connected with', address)
-    client_socket.send('Welcome!')
+    client_socket.send(bytes(f'Welcome {name}!', 'utf-8'))
     client_socket.close()
